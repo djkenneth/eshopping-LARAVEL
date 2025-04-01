78f8df8d2826ca17bfdda1be4a93c3bc528de2e0
Kenneth Ivan Pineda <40983081+djkenneth@users.noreply.github.com>
2025-04-01 13:54:27 +0800

    first commit

---
+++ resources/js/pages/Products.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+// import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Products',
+        href: '/products',
+    },
+];
+</script>
+
+<template>
+    <Head title="Products" />
+    <AppLayout :breadcrumbs="breadcrumbs">
+
+    </AppLayout>
+</template>
+
